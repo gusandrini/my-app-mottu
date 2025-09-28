d2bcae59c2e2f33e9ba29a391751e9a3a22606bd
gusandrini <gustavosandrini92@gmail.com>
2025-09-28 01:28:03 -0300

    cleaner code

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -1,17 +1,17 @@
 export const lightTheme = {
   background: "#FFFFFF",
   text: "#222222",
-  primary: "#00A651", // Verde Mottu
+  primary: "#00A651",
   secondary: "#1d3d66",
-  card: "#F5F5F5",    // ✅ Adicionado
+  card: "#F5F5F5",    
   buttonText: "#FFFFFF",
 };
 
 export const darkTheme = {
   background: "#121212",
   text: "#FFFFFF",
-  primary: "#00A651", // Verde Mottu
+  primary: "#00A651", 
   secondary: "#1d3d66",
-  card: "#1E1E1E",    // ✅ Adicionado
+  card: "#1E1E1E",   
   buttonText: "#FFFFFF",
 };
